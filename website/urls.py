27b6e84e71ef60c4b6573e940320fe6969340db7
swer <you@youraddress.com>
2022-10-01 16:33:48 +0300

    adding files

--- website/urls.py
+++ website/urls.py
@@ -3,11 +3,7 @@ from .import views
 
 urlpatterns = [
 
-    path('', views.home, name="home"),
-    path('contact.html', views.contact, name="contact"),
-    path('about.html', views.about, name="about"),
-    path('service.html', views.service, name="service"),
-    path('pricing.html', views.pricing, name="pricing"),
-    path('appointment.html', views.appointment, name="appointment"),
-    path('booknow.html', views.booknow, name="booknow"),
+    path('', views.index, name="index"),
+    path('components.html', views.components, name="components"),
+
 ]
